958627dad74092278ca593ade7ebc73b900d264b
朱明灿 <zhumingcan@zhumingcandeiMac.local>
2017-02-23 17:07:15 +0800

    Limit input

--- TextField/ViewController.m
+++ TextField/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property(strong,nonatomic)UITextField * textField;
+@property(strong,nonatomic)UITextView * textView1;
+@property(strong,nonatomic)UITextView * textView2;
 
 @end
 
@@ -17,8 +20,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.textField.placeholder = @"限制长度为5";
+    [_textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [self textView2];
+    [self textView1];
+    
 }
 
+
+-(UITextField *)textField{
+    if (!_textField) {
+        _textField = [[UITextField alloc]initWithFrame:CGRectMake(100, 50, 180, 40)];
+        _textField.delegate = self;
+        [self.view addSubview:_textField];
+    }
+    return _textField;
+}
+
+-(UITextView *)textView1{
+    if (!_textView1) {
+        _textView1 = [[UITextView alloc]initWithFrame:CGRectMake(60, 100, 200, 200)];
+        _textView1.backgroundColor = [UIColor brownColor];
+        [self.view addSubview:_textView1];
+    }
+    return _textView1;
+}
+-(UITextView *)textView2{
+    if (!_textView2) {
+        _textView2 = [[UITextView alloc]initWithFrame:CGRectMake(60, 350, 200, 150)];
+        _textView2.backgroundColor = [UIColor brownColor];
+        [self.view addSubview:_textView2];
+    }
+    return _textView2;
+}
+-(void)textFieldDidChange:(UITextField *)textField{
+    NSString * str = textField.text;
+    NSString * lang = textField.textInputMode.primaryLanguage;
+    if ([lang isEqualToString:@"zh-Hans"]) {
+        UITextRange * selectedRange = [textField markedTextRange];
+        if (!selectedRange && str.length>5) {
+            textField.text = [str substringToIndex:5];
+        }
+    }else if (str.length>5){
+        textField.text = [str substringToIndex:5];
+    }
+}
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [_textField resignFirstResponder];
+    [_textView1 resignFirstResponder];
+    [_textView2 resignFirstResponder];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
